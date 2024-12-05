6c5117a39ef27783ab3d43582c93f174489cef4e
svalentinaog <svalentinaog10@gmail.com>
2024-12-05 03:07:30 -0500

    Configuración inicial de Firebase

---
+++ lib/firebase.ts
@@ -0,0 +1,55 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+  FacebookAuthProvider,
+  getAuth,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBAOGtkq9UqqnGAUgXN8l9WLVIqZLAD-oY",
+  authDomain: "app-vet-pet-ff124.firebaseapp.com",
+  projectId: "app-vet-pet-ff124",
+  storageBucket: "app-vet-pet-ff124.firebasestorage.app",
+  messagingSenderId: "914502278527",
+  appId: "1:914502278527:web:75135a52cbb2962a860e4a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Obtener autenticación y Firestore
+export const auth = getAuth(app);
+
+export default app;
+
+// ================ Auth Functions ================ //
+
+// ================ Sign In - Email and Password ================ //
+export const signIn = (user: { email: string; password: string }) => {
+  return signInWithEmailAndPassword(auth, user.email, user.password);
+};
+
+// export const signIn = (
+//   user: { email?: string; password?: string },
+//   providerType?: "google" | "facebook"
+// ) => {
+//   if (providerType) {
+//     // Sign In with Google or Facebook
+//     const provider =
+//       providerType === "google"
+//         ? new GoogleAuthProvider()
+//         : new FacebookAuthProvider();
+//     return signInWithPopup(auth, provider);
+//   } else if (user.email && user.password) {
+//     // Sign In with Email and Password
+//     return signInWithEmailAndPassword(auth, user.email, user.password);
+//   } else {
+//     throw new Error("Invalid parameters");
+//   }
+// };
