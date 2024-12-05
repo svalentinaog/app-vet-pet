e2242efdcae5276640fe1ab94b6a4a02b15380a2
svalentinaog <svalentinaog10@gmail.com>
2024-12-04 20:49:39 -0500

    refactor: se limpió la configuración inicial del proyecto para chatbot AI
    - Se eliminaron archivos y componentes innecesarios del template predeterminado de Next.js.
    - Se reorganizaron algunos archivos y carpetas para mejorar la estructura del proyecto.

---
+++ app/chatbot/page.tsx
@@ -0,0 +1,104 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
+import { AttachmentIcon, BotIcon, UserIcon } from "@/components/icons";
+import { useChat } from "ai/react";
+import { useEffect, useRef } from "react";
+import { toast } from "sonner";
+import { Markdown } from "@/components/markdown";
+
+export default function Home() {
+  const { messages, input, handleSubmit, handleInputChange, isLoading } =
+    useChat({
+      onError: () =>
+        toast.error("You've been rate limited, please try again later!"),
+    });
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  return (
+    <div className="flex flex-row justify-center pb-20 h-dvh bg-white dark:bg-zinc-900">
+      <div className="flex flex-col justify-between gap-4">
+        {messages.length > 0 ? (
+          <div className="flex flex-col gap-2 h-full w-dvw items-center overflow-y-scroll">
+            {messages.map((message, index) => (
+              <div
+                key={message.id}
+                className={`flex flex-row gap-2 px-4 w-full md:w-[500px] md:px-0 ${
+                  index === 0 ? "pt-20" : ""
+                }`}
+              >
+                <div className="size-[24px] flex flex-col justify-center items-center flex-shrink-0 text-zinc-400">
+                  {message.role === "assistant" ? <BotIcon /> : <UserIcon />}
+                </div>
+
+                <div className="flex flex-col gap-1">
+                  <div className="text-zinc-800 dark:text-zinc-300 flex flex-col gap-4">
+                    <Markdown>{message.content}</Markdown>
+                  </div>
+                </div>
+              </div>
+            ))}
+            {isLoading &&
+              messages[messages.length - 1].role !== "assistant" && (
+                <div className="flex flex-row gap-2 px-4 w-full md:w-[500px] md:px-0">
+                  <div className="size-[24px] flex flex-col justify-center items-center flex-shrink-0 text-zinc-400">
+                    <BotIcon />
+                  </div>
+                  <div className="flex flex-col gap-1 text-zinc-400">
+                    <div>hmm...</div>
+                  </div>
+                </div>
+              )}
+            <div ref={messagesEndRef} />
+          </div>
+        ) : (
+          <div className="h-[350px] px-4 w-full md:w-[500px] md:px-0 pt-20">
+            <div className="border rounded-lg p-6 flex flex-col gap-4 text-zinc-500 text-sm dark:text-zinc-400 dark:border-zinc-700">
+              <p className="flex flex-row justify-center gap-4 items-center text-zinc-900 dark:text-zinc-50">
+                {/* 📌 CAMBIAR ESTOS ICONOS POR ICONOS RELACIONADOS A CHATS VETERINARIOS */}
+                {/* <VercelIcon /> <AttachmentIcon /> */}{" "}
+              </p>
+              <p className="text-zinc-900 dark:text-zinc-50">
+                Hola bienvenid@👋
+              </p>
+              <p>Realiza la consulta que necesitas.</p>
+            </div>
+          </div>
+        )}
+        <form
+          className="flex flex-col gap-2 relative items-center"
+          onSubmit={handleSubmit}
+        >
+          <div className="flex items-center w-full md:max-w-[500px] max-w-[calc(100dvw-32px)] bg-zinc-100 dark:bg-zinc-700 rounded-full px-4 py-2">
+            <input
+              ref={inputRef}
+              className="bg-transparent flex-grow outline-none text-zinc-800 dark:text-zinc-300 placeholder-zinc-400"
+              placeholder="Envia tu mensaje..."
+              value={input}
+              onChange={handleInputChange}
+            />
+            {/* 📌 CAMBIAR ICONO A AVIONCITO */}
+            <div
+              className="text-zinc-500 dark:text-zinc-300 hover:text-zinc-700 dark:hover:text-zinc-100 focus:outline-none mr-3 cursor-default"
+              aria-label="Upload Files"
+            >
+              <span className="w-5 h-5">
+                <AttachmentIcon aria-hidden="true" />
+              </span>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,7 @@ import { Metadata } from "next";
 import { Toaster } from "sonner";
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://ai-sdk-preview-attachments.vercel.dev"),
-  title: "Attachments Preview",
-  description: "Experimental preview of attachments in useChat hook",
+  title: "VETPET",
 };
 
 export default function RootLayout({
