7ff48e5b71a76b70d42aa32b244bdc3b9af8dfda
svalentinaog <svalentinaog10@gmail.com>
2024-12-10 09:42:13 -0500

    changes components / views

---
+++ app/report/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Report() {
+  return (
+    <div>
+      <h1>Formulario de Reportar Animal</h1>
+
+      <button>
+        <Link href={"/"}>👉Volver al home</Link>
+      </button>
+    </div>
+  );
+}
