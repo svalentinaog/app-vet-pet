82e2a442f60a4fd99b05369fb09a428381e48e62
svalentinaog <svalentinaog10@gmail.com>
2024-12-10 09:53:48 -0500

    Variables de entorno - cloudinary

--- lib/cloudinary.ts
+++ lib/cloudinary.ts
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name: "dii3p8umt",
-  api_key: "515252357957162",
-  api_secret: "<your_api_secret>",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export { cloudinary };
+export default cloudinary;
