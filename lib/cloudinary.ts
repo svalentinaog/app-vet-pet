b78988d3be59ebaf553b2f1a30ec2af331ffaffd
svalentinaog <svalentinaog10@gmail.com>
2024-12-10 09:36:07 -0500

    feat: Configurarción Cloudinary para subir imágenes desde el servidor
    - Se creó un archivo independiente en lib/cloudinary.ts para la configuración de Cloudinary.
    - Se implementó la funcionalidad para subir imágenes desde el servidor en app/upload/route.ts.
    - Se agregó manejo de errores para garantizar la estabilidad del endpoint.

---
+++ lib/cloudinary.ts
@@ -0,0 +1,9 @@
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: "dii3p8umt",
+  api_key: "515252357957162",
+  api_secret: "<your_api_secret>",
+});
+
+export { cloudinary };
