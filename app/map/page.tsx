8708f0a6bac756fae025eaa3ec3cebf09dfe6400
svalentinaog <svalentinaog10@gmail.com>
2024-12-12 01:01:05 -0500

    chore(2): remove Yarn and configure project for pnpm

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import Link from "next/link";
 import Map from "ol/Map";
 import View from "ol/View";
 import TileLayer from "ol/layer/Tile";
