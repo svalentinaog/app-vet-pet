46734125c558c1e8edc634c2a65f883dc0ccec2b
Miguel Angel <miguel.delta21@gmail.com>
2024-12-12 19:07:59 -0500

    Datos añadidos (image: array[], phone: string, status: string) a la interfaces typesReport.ts

--- interfaces/typesReport.ts
+++ interfaces/typesReport.ts
@@ -1,9 +1,11 @@
   //TODO Reporte para la lista de historial:
   export interface Report {
     id: string;                   
-    type: "lost" | "stray";       
-    age : string;
-    description: string;          
-    date: Date;                   
+    name: string;
+    phone: string,
     location?: string;            
+    description: string;          
+    age : string;
+    status: string;
+    images: string[]               
   }
