e6cfbb90072bf235668e73686b7ab9c6fc2f9db5
svalentinaog <svalentinaog10@gmail.com>
2024-12-14 01:52:58 -0500

    dev navbar

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,11 +1,21 @@
 "use client";
 
 import * as React from "react";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import { AppBar, Toolbar } from "@mui/material";
-import { Box, Container } from "@mui/system";
-import AdbIcon from "@mui/icons-material/Adb";
+import {
+  AppBar,
+  Toolbar,
+  Box,
+  Container,
+  Tabs,
+  Tab,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
+import MenuOpenIcon from "@mui/icons-material/MenuOpen";
+import Image from "next/image";
 import { MainButton } from "@/styles/mui";
 import Link from "next/link";
 
@@ -20,7 +30,7 @@ function LinkTab(props: LinkTabProps) {
       <Tab
         component="a"
         aria-current={
-          props.href === window.location.pathname ? "page" : undefined
+          window.location.pathname === props.href ? "page" : undefined
         }
         {...props}
         sx={{ color: "white" }}
@@ -31,39 +41,86 @@ function LinkTab(props: LinkTabProps) {
 
 export default function Navbar() {
   const [value, setValue] = React.useState(0);
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
 
   const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
 
+  const toggleDrawer = (open: boolean) => () => {
+    setDrawerOpen(open);
+  };
+
+  const drawerContent = (
+    <Box
+      sx={{ width: 250, height: "100vh", background: "var(--primary-color)" }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+    >
+      <List>
+        <ListItem>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Image
+              src="/assets/logo.svg"
+              alt="Logo"
+              width={100}
+              height={0}
+              layout="intrinsic"
+            />
+          </Box>
+        </ListItem>
+
+        {[
+          { label: "Inicio", href: "/" },
+          { label: "Informar", href: "#report" },
+          { label: "Consultar", href: "/chatbot" },
+          { label: "Localizar", href: "/map" },
+        ].map((item, index) => (
+          <ListItem key={index} component={Link} href={item.href}>
+            <ListItemText
+              primary={item.label}
+              sx={{ color: "var(--light-color)" }}
+            />
+          </ListItem>
+        ))}
+        <ListItem>
+          <MainButton fullWidth>INICIAR SESIÓN</MainButton>
+        </ListItem>
+      </List>
+    </Box>
+  );
+
   return (
     <AppBar
       position="fixed"
-      sx={{
-        background: "transparent",
-        padding: "1em",
-        boxShadow: "inherit",
-      }}
+      sx={{ background: "transparent", padding: "1em", boxShadow: "inherit" }}
     >
       <Container maxWidth="xl">
         <Toolbar
           disableGutters
           sx={{
             width: "100%",
-            display: "flex",
             justifyContent: "space-between",
             alignItems: "center",
           }}
         >
+          {/* Icono de la App (Logo)*/}
           <Box>
-            <AdbIcon />
+            <Image
+              src="/assets/logo.svg"
+              alt="Logo"
+              width={100}
+              height={0}
+              layout="intrinsic"
+            />
           </Box>
+
+          {/* Navbar Tabs (Desktop) */}
           <Box
             sx={{
-              width: "50%",
-              display: "flex",
-              gap: 6,
+              display: { xs: "none", md: "flex" },
               alignItems: "center",
+              gap: 6,
             }}
           >
             <Tabs
@@ -71,8 +128,6 @@ export default function Navbar() {
               onChange={handleChange}
               textColor="secondary"
               indicatorColor="secondary"
-              aria-label="nav tabs example"
-              role="navigation"
             >
               <LinkTab label="Inicio" href="/" />
               <LinkTab label="Informar" href="#report" />
@@ -82,7 +137,23 @@ export default function Navbar() {
 
             <MainButton sx={{ maxWidth: "200px" }}>INICIAR SESIÓN</MainButton>
           </Box>
+
+          {/* Menu de Hamburguesa (Mobile) */}
+          <Box sx={{ display: { xs: "block", md: "none" } }}>
+            <IconButton
+              onClick={toggleDrawer(true)}
+              color="inherit"
+              sx={{ color: "var(--light-color)" }}
+            >
+              <MenuOpenIcon />
+            </IconButton>
+          </Box>
         </Toolbar>
+
+        {/* Drawer para el Menú de Navegación en Mobile */}
+        <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
+          {drawerContent}
+        </Drawer>
       </Container>
     </AppBar>
   );
