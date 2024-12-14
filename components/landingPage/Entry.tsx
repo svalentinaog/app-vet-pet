59800193e8fe42bf6faa72b5299e11ccd728eda1
svalentinaog <svalentinaog10@gmail.com>
2024-12-14 03:54:11 -0500

    quick reload

--- components/landingPage/Entry.tsx
+++ components/landingPage/Entry.tsx
@@ -2,7 +2,7 @@
 
 import { MainButton } from "@/styles/mui";
 import { Typography } from "@mui/material";
-import { Box } from "@mui/system";
+import { Box, Container } from "@mui/system";
 import React from "react";
 
 function Entry() {
@@ -29,55 +29,57 @@ function Entry() {
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
-          padding: { xs: "3em", sm: "5em", md: "15em" },
+          padding: { xs: "8em 1em 6em", sm: "5em", md: "5em" },
         }}
       >
-        <Box
-          sx={{
-            width: { xs: "90%", sm: "60%", md: "50%" },
-            display: "flex",
-            flexDirection: "column",
-            gap: 4,
-            justifyContent: "flex-start",
-          }}
-        >
+        <Container maxWidth="xl">
           <Box
             sx={{
+              width: { xs: "90%", sm: "60%", md: "50%" },
               display: "flex",
               flexDirection: "column",
-              gap: 2,
+              gap: 4,
+              justifyContent: "flex-start",
             }}
           >
-            <Typography
-              variant="h3"
+            <Box
               sx={{
-                lineHeight: "1.5em",
-                fontWeight: "bold",
-                color: "var(--light-color)",
-                fontSize: { xs: "1.5em", sm: "2em", md: "2.5em" },
+                display: "flex",
+                flexDirection: "column",
+                gap: 2,
               }}
             >
-              Bienvenido a VetPet <br />
-              ¡Nos encanta tenerte aquí!
-            </Typography>
+              <Typography
+                variant="h3"
+                sx={{
+                  lineHeight: "1.5em",
+                  fontWeight: "bold",
+                  color: "var(--light-color)",
+                  fontSize: { xs: "1.5em", sm: "2em", md: "2.5em" },
+                }}
+              >
+                Bienvenido a VetPet <br />
+                ¡Nos encanta tenerte aquí!
+              </Typography>
+            </Box>
+            <MainButton
+              sx={{
+                maxWidth: { xs: "150px", sm: "300px", md: "300px" },
+                fontSize: { xs: "1em", sm: "1.5em" },
+              }}
+            >
+              Ver Amigos
+            </MainButton>
           </Box>
-          <MainButton
+          <Box
             sx={{
-              maxWidth: { xs: "150px", sm: "300px", md: "300px" },
-              fontSize: { xs: "1em", sm: "1.5em" },
+              width: { xs: "0", sm: "50%" },
+              display: { xs: "none", sm: "block" },
             }}
           >
-            Ver Amigos
-          </MainButton>
-        </Box>
-        <Box
-          sx={{
-            width: { xs: "0", sm: "50%" },
-            display: { xs: "none", sm: "block" },
-          }}
-        >
-          {/* Espacio vacío para division */}
-        </Box>
+            {/* Espacio vacío para division */}
+          </Box>
+        </Container>
       </Box>
     </Box>
   );
